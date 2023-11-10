225ea78376b94be9f8e5401004c424370fc9aabc
Joseph <1josephasare1@gmail.com>
2023-11-10 17:47:13 +0000

    Pages linking

--- FinalYearProject/src/app/app-routing.module.ts
+++ FinalYearProject/src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ExercisesPage } from './exercises/exercises.page';
+import { ProfilePage } from './profile/profile.page';
+import { ChatbotPage } from './chatbot/chatbot.page';
+
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    
   },
+  { path: 'chatbot', component: ChatbotPage },
+  { path: 'profile', component: ProfilePage },
+  { path: 'exercises', component: ExercisesPage },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   { path: 'register', loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationPageModule) },
   {
@@ -16,6 +24,18 @@ const routes: Routes = [
     path: '',
     redirectTo: 'login',
     pathMatch: 'full'
+  },
+  {
+    path: 'exercises',
+    loadChildren: () => import('./exercises/exercises.module').then( m => m.ExercisesPageModule)
+  },
+  {
+    path: 'chatbot',
+    loadChildren: () => import('./chatbot/chatbot.module').then( m => m.ChatbotPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
 ];
 
