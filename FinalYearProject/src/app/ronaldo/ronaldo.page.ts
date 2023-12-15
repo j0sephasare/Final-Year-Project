971556c6c3106ab1c647a5574abc43658b57eeb9
Joseph Asare <99197406+j0sephasare@users.noreply.github.com>
2023-12-15 20:29:30 +0000

    Additions to styling and Ronaldo page

--- FinalYearProject/src/app/ronaldo/ronaldo.page.ts
+++ FinalYearProject/src/app/ronaldo/ronaldo.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-ronaldo',
@@ -9,20 +10,38 @@ export class RonaldoPage implements OnInit {
   exercises = [
     { name: 'Exercise 1', done: false },
     { name: 'Exercise 2', done: false },
+    { name: 'Exercise 3', done: false },
     // Add more exercises here
   ];
 
-  constructor() { }
+  constructor(private alertController: AlertController) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   allExercisesDone() {
-    return this.exercises.every(exercise => exercise.done);
+    return this.exercises.every((exercise) => exercise.done);
   }
 
-  completeExercises() {
-    // Logic when all exercises are completed
-    console.log('All exercises completed!');
+  async completeTraining() {
+    if (this.allExercisesDone()) {
+      // Logic when all exercises are completed
+      await this.showTrainingCompletedPopup();
+    } else {
+      console.log('Not all exercises are completed yet.');
+    }
+  }
+
+  markExerciseAsCompleted(index: number) {
+    this.exercises[index].done = true;
+  }
+
+  async showTrainingCompletedPopup() {
+    const alert = await this.alertController.create({
+      header: 'Training Completed',
+      message: 'Congratulations! You have completed your training.',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
   }
 }
