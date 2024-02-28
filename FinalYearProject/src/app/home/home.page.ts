d32bc75a0a019aab663fd5d77a8fb9c76dad3426
j0sephasare <1josephasare1@gmail.com>
2024-02-28 23:46:53 +0000

    exercises outputting to homepage

--- FinalYearProject/src/app/home/home.page.ts
+++ FinalYearProject/src/app/home/home.page.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserExerciseService } from '../user-exercise.service'; // Import your service
+import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  savedWorkouts: any[] = []; // Use a type here if you have one
 
-  constructor() { }
+  constructor(private userExerciseService: UserExerciseService, public auth: AuthService) { } // Inject your service
 
   ngOnInit() {
     this.updateDateTime();
     setInterval(() => this.updateDateTime(), 1000);
+    this.fetchSavedWorkouts(); // Fetch the workouts when the component loads
+  }
+
+  fetchSavedWorkouts() {
+    this.auth.user$.subscribe((user) => {
+      if (user && user.sub) {
+        // Call the service method to get saved exercises for the logged-in user
+        this.userExerciseService.getSavedExercises(user.sub).subscribe({
+          next: (workouts) => {
+            this.savedWorkouts = workouts;
+          },
+          error: (error) => {
+            console.error('Error fetching saved workouts:', error);
+          }
+        });
+      }
+    });
   }
 
   updateDateTime() {
