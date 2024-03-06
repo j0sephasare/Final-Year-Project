f59f3fb22ee7f90c68b8518bee38cfc4ef4dd30c
j0sephasare <1josephasare1@gmail.com>
2024-03-06 21:04:34 +0000

    Mobile application testing #3

--- FinalYearProject/src/app/app.component.ts
+++ FinalYearProject/src/app/app.component.ts
@@ -1,32 +1,44 @@
-// Import Capacitor's App object
+// Import Capacitor's App object and Auth0 AuthService
 import { App } from '@capacitor/app';
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular'; // Import AuthService
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private router: Router
+    private router: Router,
+    private auth: AuthService // Inject AuthService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Handle the logic for when the app is opened with a deep link
+      // Handle deep links
       App.addListener('appUrlOpen', (event: { url: string }) => {
-        // Example URL: com.FinalYearProject.app://callback?code=123
         const url = new URL(event.url);
-        const pathname = url.pathname;
-        // Assuming you have a route in your Angular app that corresponds
-        // to the path in the deep link, navigate to it
-        this.router.navigateByUrl(pathname);
+        // Instead of directly navigating, check the authentication state
+        this.checkAuthenticationState();
       });
+    });
+  }
+
+  checkAuthenticationState() {
+    // Check if user is authenticated
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        // Navigate to the home page if authenticated
+        this.router.navigate(['/home']);
+      } else {
+        // Navigate to login if not authenticated
+        this.router.navigate(['/login']);
+      }
     });
   }
 }
