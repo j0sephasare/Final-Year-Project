5a87268c2f5965c1a9c7477a5cfce5f1c6ffc880
Joseph Asare <99197406+j0sephasare@users.noreply.github.com>
2024-03-19 17:31:46 +0000

    Android settings

--- FinalYearProject/src/environments/environment.prod.ts
+++ FinalYearProject/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyBFKHhS54DeEcG48bxQxE6J7HjOWpXUCNU",
+    authDomain: "fireappconnect-43914.firebaseapp.com",
+    projectId: "fireappconnect-43914",
+    storageBucket: "fireappconnect-43914.appspot.com",
+    messagingSenderId: "360795138070",
+    appId: "1:360795138070:web:9eab83bef88b6566ba2d37",
+    measurementId: "G-NPYRR05D5N"
+  }
+
 };
