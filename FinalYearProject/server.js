e6050c49df3e6e408ad8589a0eabaa3e7bea3734
Joseph Asare <99197406+j0sephasare@users.noreply.github.com>
2024-04-22 15:24:02 +0100

    Heroku push using express server to build

--- FinalYearProject/server.js
+++ FinalYearProject/server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + 'www'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/www/index.html'));
+  res.sendFile(path.join(__dirname+'www/index.html'));
 });
 
 app.listen(port, () => {
