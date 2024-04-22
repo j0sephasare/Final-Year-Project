1064ccd1504fdf9c7d8a473b3c6e330ae3fb03aa
Joseph Asare <99197406+j0sephasare@users.noreply.github.com>
2024-04-22 13:37:00 +0100

    added Profile for Heroku

---
+++ FinalYearProject/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/www'));
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/www/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
